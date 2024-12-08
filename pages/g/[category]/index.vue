1a863ff5c798996f855bc1c29f892dd293607972
jyhyun1008 <jayhyun1008@gmail.com>
2024-12-08 10:28:05 +0900

    카테고리 전체보기 추가

--- pages/g/[category]/index.vue
+++ pages/g/[category]/index.vue
@@ -3,7 +3,7 @@
         <h1 id="title"><a href="/">피치타르트 블로그</a></h1>
         <h2 id="category-title">갤러리 / {{ route.params.category }}</h2>
         <div style="display: flex; align-items: center; gap: 12px">
-            다른 카테고리 : 
+            다른 카테고리 : <a href="/g/">전체보기</a>
             <div v-for="category of categories" class="category-list">
                 <a :href="`/g/${category}`">{{ category }}</a>
             </div>
